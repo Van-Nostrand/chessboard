1a05f389ff4164b25b6794d3c3d7f6e3dffc760f
Van-Nostrand <dmdoull43@gmail.com>
2020-08-10 13:03:40 -0700

    more testing on SVG elements

--- src/CanvasDrawTest.js
+++ src/CanvasDrawTest.js
@@ -15,75 +15,126 @@ const CanvasDrawTest = (props = {}) => {
   } = props;
 
   const pieceBoardRef = useRef(null);
+  const multiRef1 = useRef(null);
+  const multiRef2 = useRef(null);
 
   const BISHOP = new Path2D(PIECE_SVG.BISHOP_SVG);
-  const PIECESCALE = 0.6;
-  const PIECE_TRANSLATE = [ -10 , 0 ];
+  const PIECESCALE = 0.58;
 
   const PIECETEST = {
     piece1: {svg: PIECE_SVG.BISHOP_SVG, x: 0, y: 0},
-    piece2: {svg: PIECE_SVG.QUEEN_SVG, x: 50, y: 50},
-    piece3: {svg: PIECE_SVG.KING_SVG, x: 100, y: 100},
-    piece4: {svg: PIECE_SVG.PAWN_SVG, x: 150, y: 150}
+    piece2: {svg: PIECE_SVG.QUEEN_SVG, x: 60, y: 0},
+    piece3: {svg: PIECE_SVG.KING_SVG, x: 120, y: 0},
+    piece4: {svg: PIECE_SVG.PAWN_SVG, x: 180, y: 0}
   };
 
   const drawPiece = (context, location, SVG) => {
     
+    context.moveTo(location[0],location[1]);
     context.translate(location[0],location[1]);
     context.fill(new Path2D(SVG));
-    // context.translate(-location[0], -location[1]);
+    context.restore();
+  }
+ 
+  const lineTest = (context, location, SVG) => {
+    context.beginPath();
+    context.moveTo(location[0],location[1]);
+    context.translate(location[0],location[1]);
+    context.closePath();
+    context.stroke(new Path2D(SVG));
     context.restore();
   }
 
-  useEffect(() => {
-    const context = pieceBoardRef.current.getContext('2d');
-    
-    // this "saves" the current state of context
-    context.scale(PIECESCALE, PIECESCALE);
-    // context.moveTo(0,0);
-    context.lineWidth = -1;
-    context.fillStyle = "#333333";
-    context.save();
-    
-    for(const piece in PIECETEST){
-      drawPiece(context, [PIECETEST[piece].x, PIECETEST[piece].y], PIECETEST[piece].svg);
-      // context.restore();
+  const drawGrid = (context) => {
+    context.strokeWidth = 1;
+    context.strokeStyle = "blue";
+    for(let i = 0; i < 10; i++){
+      context.beginPath();
+      context.moveTo(i*tileSize,0);
+      context.lineTo(i*tileSize,480);
+      context.closePath();
+      context.stroke();
     }
-
-    // stroke is unnecessary unless you want your paths to be outlines. with path elements, fill will draw like a pen or paintbrush. 
-
-    // this restores the saved context state
-    // in this case, it restores a blank canvas
     context.restore();
+    context.strokeWidth = 1;
+    context.strokeStyle = "blue";
+    for(let i = 0; i < 10; i++){
+      context.beginPath();
+      context.moveTo(0,i*tileSize);
+      context.lineTo(480,i*tileSize);
+      context.closePath();
+      context.stroke();
+    }
+    context.restore();
+  }
+
+  const canvasClick = (e) => {
+    console.log(`just clicked ${e.target.id}`);
+    console.log(e.clientX);
+    let coordinates = [e.clientX, e.clientY];
+  }
+
+  useEffect(() => {
+    // const context = pieceBoardRef.current.getContext('2d');
+    const context1 = multiRef1.current.getContext('2d');
+    const context2 = multiRef2.current.getContext('2d');
+    
+    //reposition canvas on the screen
+    multiRef1.current.style.left = "100px";
+
+
+    context1.fillStyle = "blue";
+    context1.scale(PIECESCALE,PIECESCALE);
+    context1.translate(-3, 0);
+    context1.fill(new Path2D(PIECETEST.piece1.svg));
+    
+    // The SVG paths all start at different points of the shape, so they have different "origins" and require different translations
+    context2.fillStyle = "red";
+    context2.translate(-24,-61);
+    context2.scale(PIECESCALE,PIECESCALE);
+    context2.fill(new Path2D(PIECETEST.piece2.svg));
+
   })
 
   const pieceBoardStyle = {
     padding: 0,
     margin: 0,
-    position: "relative"
+    position: "relative",
+    border: "1px solid red",
+    top: "100px"
   }
 
-  // let divStyle = {
-  //   backgroundImage: `url(${piecesIMG})`,
-  //   backgroundSize: "1200px",
-  //   backgroundPosition: "50px 0",
-  //   width: "300px",
-  //   height: "300px",
-  //   position: "absolute",
-  //   // transform: "translate(50px, -200px)"
-  //   top: 0,
-  //   left: 0
-  // }
+  const multiStyle = {
+    padding: 0,
+    margin: 0,
+    position: "absolute",
+    border: "2px solid green",
+    display: "inline-block"
+  }
+
+  const multiCanvas = [
+    <canvas
+      ref={multiRef1}
+      id='canvas1'
+      key="canvas1"
+      width="60"
+      height="60"
+      style={multiStyle}
+      onClick={canvasClick} />,
+    <canvas
+      ref={multiRef2}
+      id="canvas2"
+      key="canvas2"
+      width="60"
+      height="60"
+      style={multiStyle}
+      onClick={canvasClick} />
+  ]
+
 
   return (
     <div>
-      <canvas 
-        id="piece-board"
-        ref={pieceBoardRef}
-        width={tileSize * boardWidth}
-        height={tileSize * boardHeight}
-        style={pieceBoardStyle} />
-      {/* <div style={divStyle}></div> */}
+      {multiCanvas}
     </div>
   )
 
